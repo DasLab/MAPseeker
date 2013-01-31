c7ba3b4148b1db2e9d01f06f4caf4ddbe55a9dda
rhiju <rhiju@bc2b3979-93d1-0410-b55d-9e00c09966b3>
2013-01-31 23:42:39 +0000

    one more useful file

---
+++ src/matlab/output_MAPseeker_data_to_rdat_file.m
@@ -0,0 +1,42 @@
+function rdat = output_MAPseeker_data_to_rdat_file( filename, name, D_average, D_average_err, annotations, comments, data_annotations );
+%
+% rdat = output_MAPseeker_data_to_rdat_file( filename, name, D_average, D_average_err, annotations, comments, data_annotations/RNA_info );
+%
+%  filename  = name of file to output to disk
+%  name      = will show up in NAME field of RDAT -- short description of RNA whos variants are tested.
+%  D_average = Matrix of data
+%
+% (C) R. Das, 2013
+%
+
+seqpos = [0:size(D_average,2)-1];
+
+% don't need to output fully extended band (at site "0") -- 
+% its zero after processing
+outbins = [2 : length(seqpos)]; 
+seqpos_out = seqpos( outbins ); 
+reactivity_out =  D_average(:,outbins)';
+reactivity_err_out =  D_average_err(:,outbins)';
+
+offset = 0; mutpos = [];
+
+sequence = ''; for k = 1:(size(D_average,2)-1); sequence = [sequence,'X']; end;
+structure = strrep( sequence, 'X','.');
+
+if ~iscell( data_annotations ) % assume it is an RNA_info object
+  RNA_info = data_annotations;
+  data_annotations = {};
+  for j = 1:length( RNA_info )
+    tag = RNA_info(j).Header;    
+    data_annotations{j} = { ['name:',tag],  ['sequence:',RNA_info(j).Sequence] };
+  end
+end
+
+rdat = output_workspace_to_rdat_file( filename, name, sequence, offset, seqpos_out, reactivity_out, ...
+			       mutpos, structure, ...
+			       annotations, data_annotations, ...
+			       reactivity_err_out, ...
+			       [],[],[], comments );
+
+rdat = show_rdat( filename );
+
