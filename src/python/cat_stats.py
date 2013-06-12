e57ac6df13e6fb6411b5dd15a125d07168925bf0
rhiju <rhiju@bc2b3979-93d1-0410-b55d-9e00c09966b3>
2013-06-12 21:53:26 +0000

    in conversion of eterna library order to RNA_sequences.fasta, print warning if length of structure does not match sequence. Make addition of P4-P6 on by default. Print out number of sequences found

--- src/python/cat_stats.py
+++ src/python/cat_stats.py
@@ -1,66 +1,7 @@
 #!/usr/bin/python
 
 import sys
-import string
-from os import system,popen
-from os.path import basename, abspath, dirname
-from glob import glob
+from cat_stats_stuff import cat_stats
 
 outdirs = sys.argv[1:]
-delete_files = False
-if '--delete' in outdirs:
-    pos =outdirs.index( '--delete')
-    del( outdirs[ pos ] )
-    delete_files = True
-
-scripts_path = dirname( abspath( sys.argv[0] ) )
-
-which_files_to_cat = {}
-
-for outdir in outdirs:
-
-    globfiles_all = glob( outdir+'/*/stats*txt' )
-
-    basenames = map( lambda x:basename(x), globfiles_all )
-    basenames = set( basenames )
-
-    print basenames
-
-    for filename in basenames:
-        globfiles = glob( outdir+'/*/'+filename )
-        N = []
-        for file in globfiles: # probably could use pickle or something.
-            lines = open( file ).readlines()
-            for i in range( len( lines ) ):
-                line = lines[i]
-                cols = string.split( line )
-                if len( N ) <= i:
-                    zero_vector = []
-                    if len( zero_vector ) == 0:
-                        for col in cols: zero_vector.append( 0.0 )
-                    N.append( zero_vector )
-                for m in range(len(cols)):
-                    k = float(cols[m])
-                    N[i][m] += k
-
-        final_file = outdir+'/'+filename
-        fid = open( final_file, 'w' )
-        for row in N:
-            for m in row: fid.write( '%11.4f' % m )
-            fid.write( '\n' )
-        fid.close()
-
-        system( 'cp %s ./' % final_file )
-        print 'Created: ', basename(final_file)
-
-    if ( delete_files ): # remove dirs.
-        dirs_to_delete = []
-        for file in globfiles_all:
-            if not dirname( file ) in dirs_to_delete: dirs_to_delete.append( dirname(file) )
-        command = 'rm -rf '+string.join( dirs_to_delete  )
-        system( command )
-
-        # clogs up system -- and will affect any future runs...
-        command = 'rm -rf dagMAPSEEKER.*'
-        system( command )
-
+cat_stats( outdirs )
--- src/python/get_RNA_sequences_from_EteRNA_file.py
+++ src/python/get_RNA_sequences_from_EteRNA_file.py
@@ -4,14 +4,14 @@ from sys import argv
 
 def Help():
     print
-    print argv[0], ' EteRNAfile'
+    print argv[0], ' EteRNAfile [-no_P4P6]'
     print
     exit()
 
 if len( argv ) < 2: Help()
 
-ADD_P4P6 = False
-if '-addP4P6' in argv: ADD_P4P6 = True
+NO_P4P6 = False
+if '-no_P4P6' in argv: NO_P4P6 = True
 
 EteRNAfile = argv[1]
 
@@ -26,16 +26,19 @@ lines = open( EteRNAfile ).readlines()
 if len( lines ) == 1:
     lines = lines[0].split( '\r' )
 
-if ADD_P4P6:
+if not NO_P4P6:
     lines.append( '0\tP4-P6 domain, Tetrahymena ribozyme\t0\tGGCCAAAGGCGUCGAGUAGACGCCAACAACGGAAUUGCGGGAAAGGGGUCAACAGCCGUUCAGUACCAAGUCUCAGGGGAAACUUUGAGAUGGCCUUGCAAAGGGUAUGGUAAUAAGCUGACGGACAUGGUCCUAACCACGCAGCCAAGUCCUAAGUCAACAGAUCUUCUGUUGAUAUGGAUGCAGUUCAAAACCAAACCGUCAGCGAGUAGCUGACAAAAAGAAACAACAACAACAAC\tP4-P6 with double reference hairpin\t.......((((((.....))))))...........((((((...((((((.....(((.((((.(((..(((((((((....)))))))))..((.......))....)))......)))))))....))))))..)).))))((...((((...(((((((((...)))))))))..))))...)).............((((((.....))))))......................\n' )
 
 
 tag_map = {}
+count = 0
 for line in lines:
     cols = line.replace('\n','').split( '\t' );
+    if len( cols ) < 5: continue
     header = '> '+cols[0]+'\t'+ cols[1] + '\t' + cols[4]
     sequence = cols[3]
     structure = cols[5]
+    if not len( sequence ) == len( structure ): print 'WARNING! Length( sequence ) != length(structure): ',line
 
     fid1.write( header+'\n')
     fid1.write( sequence+'\n\n')
@@ -44,9 +47,11 @@ for line in lines:
     fid2.write( sequence+'\n')
     fid2.write( structure+'\n\n')
 
+    count = count + 1
+
 fid1.close()
 fid2.close()
 
-
+print 'Found ', count, ' sequences.'
 print 'Created: ', outfile1
 print 'Created: ', outfile2
