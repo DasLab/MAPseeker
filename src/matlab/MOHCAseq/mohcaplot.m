10b81291febff1b56d2ea1282180d714fc6929f7
Clarence Y Cheng <cyucheng28@gmail.com>
2014-05-17 16:26:05 -0700

    Update mohcaplot to accept secondary structure and overlay on proximity map.

--- src/matlab/MOHCAseq/mohcaplot.m
+++ src/matlab/MOHCAseq/mohcaplot.m
@@ -1,4 +1,4 @@
-function mohcaplot( D, seqpos, ligpos, titl, ticksize, save_path, pdb_path, contours )
+function mohcaplot( D, seqpos, ligpos, titl, ticksize, save_path, secstr, pdb_path, contours )
 
 % Plots 2D maps in MOHCA style
 %
@@ -9,13 +9,14 @@ function mohcaplot( D, seqpos, ligpos, titl, ticksize, save_path, pdb_path, cont
 %       titl    = desired plot title, string
 %       ticksize  = font size of tick labels
 %       save_path = path to save file (including filename)
+%       secstr    = cell array with {sequence, secstr, offset, data_types, numlanes}
 %       pdb_path  = path of PDB file, provides axis limits for showing ROI only 
 %       contours  = enter 1 if want to plot contours from pdb file; default does not plot contours 
 %
 % Clarence Cheng, 2014
 %
 
-if ~exist( 'ticksize', 'var' ); ticksize = 25; end
+if ~exist( 'ticksize', 'var' ) || isempty( ticksize ); ticksize = 25; end
 if ~exist( 'titl', 'var' ); titl = ''; end
 % if ~exist( 'contours', 'var' ); contours = ''; end
 
@@ -40,6 +41,46 @@ hold on;
 % Add title
 title( titl, 'fonts', 15, 'fontw', 'bold' );
 
+% Rotate xticklabels and reposition
+xticklabel = get(gca,'XTickLabel');
+set(gca,'XTickLabel','');
+hxLabel=get(gca,'XLabel');
+set(hxLabel,'Units','data');
+xLabelPosition=get(hxLabel,'Position');
+y=xLabelPosition(2) - 7;
+XTick=str2num(xticklabel)+1;
+y=repmat(y,length(XTick),1);
+fs = get(gca,'fonts');
+hText=text(XTick,y,xticklabel,'fonts',ticksize,'fontw','bold');
+set(hText,'Rotation',90,'HorizontalAlignment','right');
+xlab=get(gca,'XLabel');
+set(xlab,'Position',get(xlab,'Position') + [0 7 0]);
+
+% Make colorbar legend
+hc = colorbar('location','eastoutside');
+hcm = max(get(hc,'YLim'))
+set(hc,'YTick',[0.5 hcm-0.5]);
+set(hc,'YTickLabel',{'0.0','1.0'});
+hcp = get(hc,'pos');
+pos = get(gca,'pos');
+set(hc,'position',[hcp(1)*0.92 hcp(2) hcp(3)*0.5 pos(4)*0.25],'fonts',25,'fontw','bold');
+
+% Overlay secondary structure
+if exist( 'secstr', 'var' )
+    seq = secstr{1};
+    str = secstr{2};
+    for i = 1:length(secstr{1})
+        data_types{i} = i;
+    end
+    area_pred = generate_area_pred(seq, str, 0, data_types, length(secstr{1}));
+        % in future, use sequence and secstruct from rdat and crop to correct size
+    [x_pred, y_pred] = find(area_pred);
+    x_pred = x_pred - 1 + seqpos(1);
+    y_pred = y_pred - 1 + ligpos(1);
+    plot(x_pred, y_pred, '.', 'color', [1 0 1]);
+end
+
+% Overlay tertiary structure contours
 if exist( 'pdb_path', 'var' )
     [D_sim, res_rad, res_hit, dist_matrix, pdbstruct] = get_simulated_data( pdb_path );
 
@@ -69,30 +110,6 @@ if exist( 'pdb_path', 'var' )
 else
     axis( [min(seqpos)-0.5 max(seqpos)+0.5 min(ligpos)-0.5 max(ligpos)+0.5 ]);
 end    
-
-% Rotate xticklabels and reposition
-xticklabel = get(gca,'XTickLabel');
-set(gca,'XTickLabel','');
-hxLabel=get(gca,'XLabel');
-set(hxLabel,'Units','data');
-xLabelPosition=get(hxLabel,'Position');
-y=xLabelPosition(2) - 7;
-XTick=str2num(xticklabel)+1;
-y=repmat(y,length(XTick),1);
-fs = get(gca,'fonts');
-hText=text(XTick,y,xticklabel,'fonts',ticksize,'fontw','bold');
-set(hText,'Rotation',90,'HorizontalAlignment','right');
-xlab=get(gca,'XLabel');
-set(xlab,'Position',get(xlab,'Position') + [0 7 0]);
-
-% Make colorbar legend
-hc = colorbar('location','eastoutside');
-hcm = max(get(hc,'YLim'));
-set(hc,'YTick',[0.5 hcm-0.5]);
-set(hc,'YTickLabel',{'0.0','1.0'});
-hcp = get(hc,'pos');
-pos = get(gca,'pos');
-set(hc,'position',[hcp(1)*0.92 hcp(2) hcp(3)*0.5 pos(4)*0.25],'fonts',25,'fontw','bold');
 
 % Save figure
 if exist( 'save_path', 'var' )
